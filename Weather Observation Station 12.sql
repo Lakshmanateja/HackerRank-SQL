50248b98065da83199553243c1d5ddc61bd7d28e
Lakshmana Teja Kapuganti <76482841+Lakshmanateja@users.noreply.github.com>
2023-02-23 14:44:24 +0530

    Weather Observation Station 12

---
+++ Weather Observation Station 12.sql
@@ -0,0 +1,5 @@
+select distinct city
+from station
+where (substr(city,1,1)!='A' and substr(city,1,1)!='E' and substr(city,1,1)!='I' and substr(city,1,1)!='O' and substr(city,1,1)!='U') 
+and 
+(substr(city,length(city),1)!='a' and substr(city,length(city),1)!='e' and substr(city,length(city),1)!='i' and substr(city,length(city),1)!='o' and substr(city,length(city),1)!='u');
